dcd63e4a858816200aa541182d42ceebd18bd7cd
Chris Olin <contact@chrisolin.com>
2013-06-26 08:56:08 -0400

    created bash script to make symlinking files in $HOME quick and easy

---
+++ link.sh
@@ -0,0 +1,16 @@
+########################################################
+# vim:smd:ar:si:et:bg=dark:ts=4:sw=4
+# file: link.sh
+# author: Chris Olin - http://chrisolin.com
+# purpose: create symlinks in ~ for all files in branch
+# created date: 06-26-2013
+# last modified: Wed, Jun 26, 2013  8:54:37 AM
+# license:
+########################################################
+#!/bin/bash
+
+SRC=`pwd`
+for i in `ls -1A`;
+do
+    ln -s $SRC/$i $HOME/$i
+done
