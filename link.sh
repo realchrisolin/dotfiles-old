48e64e9e83394f921871e6de939f63e369f76a61
Chris Olin <contact@chrisolin.com>
2017-04-12 21:20:20 -0400

    update url and blow out last modified line from link.sh

--- link.sh
+++ link.sh
@@ -1,10 +1,9 @@
 ########################################################
 # vim:smd:ar:si:noet:bg=dark:sts=0:ts=4:sw=4
 # file: link.sh
-# author: Chris Olin - http://chrisolin.com
+# author: Chris Olin - https://www.chrisolin.com
 # purpose: create symlinks in ~ for all files in branch
 # created date: 06-26-2013
-# last modified: Tue 29 Oct 2013 12:54:04 PM EDT
 # license:
 ########################################################
 #!/bin/bash
